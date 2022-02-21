992364ab5a45e4a6719e835b02069f8097b1b34a
Michał Perzak <michal.perzak@gmail.com>
2022-02-21 23:06:19 +0100

    implemented reading Dds device

--- Pi/ddsReader.py
+++ Pi/ddsReader.py
@@ -4,11 +4,17 @@ from os import makedirs
 from os.path import exists
 from random import randint
 from time import sleep
+from struct import *
+
+import minimalmodbus
+import serial
 
 outputDir = 'data'
 rtuPort = '/dev/ttyS0'
 deviceId = 13
 pollIntervalSeconds = 1
+serialBaudrate = 9600
+serialTimeout = 1.3  # seconds
 
 
 class RegisterItem:
@@ -19,13 +25,47 @@ class RegisterItem:
 
 
 def readArduinoTemp(instrument):
-    registers = 0  # instrument.read_registers(0, 2)
+    registers = instrument.read_registers(0, 2)
 
-    return {'h': registers[0], 't': registers[1]}
+    return [
+        RegisterItem('Humidity', registers[0] / 100.0, '%'),
+        RegisterItem('temperature', registers[1] / 100.0, 'C')
+    ]
+
+
+def regToUnsigned32(registers, startIdx):
+    s = pack('>HH', registers[startIdx], registers[startIdx+1])
+    return unpack('>L', s)[0]
+
+
+def regToSigned16(registers, index):
+    s = pack('>H', registers[index])
+    return unpack('>h', s)[0]
 
 
 def readDds238(instrument):
-    return None
+    registers = instrument.read_reagisters(0, 18)
+
+    return [
+        RegisterItem('total energy',
+                     regToUnsigned32(registers, 0) * 0.01, 'kWh'),
+        RegisterItem('export energy',
+                     regToUnsigned32(registers, 8) * 0.01, 'kWh'),
+        RegisterItem('import energy',
+                     regToUnsigned32(registers, 10) * 0.01, 'kWh'),
+        RegisterItem('voltage',
+                     registers[12] * 0.1, 'V'),
+        RegisterItem('current',
+                     registers[13] * 0.01, 'A'),
+        RegisterItem('active power',
+                     regToSigned16(registers, 14), 'W'),
+        RegisterItem('reactive power',
+                     registers[15], 'VAr'),
+        RegisterItem('power factor',
+                     registers[16] * 0.001, 'Scale'),
+        RegisterItem('frequency',
+                     registers[17] * 0.01, 'Hz'),
+    ]
 
 
 def readTest(instrument):
@@ -72,17 +112,26 @@ def main():
     print('- rtuPort: ', rtuPort)
     print('- deviceId: ', deviceId)
 
-    instrument = 'a'  # modbus instrument
+    instrument = minimalmodbus.Instrument(rtuPort, deviceId)
+    instrument.serial.baudrate = serialBaudrate
+    instrument.serial.timeout = serialTimeout
+    instrument.serial.bytesize = 8
+    instrument.serial.parity = serial.PARITY_NONE
+    instrument.serial.stopbit = 1
+
     print('Instrument initialized, begining to poll')
 
     if (not exists(outputDir)):
         makedirs(outputDir)
 
     while(True):
-        print('Next read')
-        data = readTest(instrument)
-        writeLineToFile(data)
         sleep(pollIntervalSeconds)
+        print('Next read')
+        try:
+            data = readTest(instrument)
+            writeLineToFile(data)
+        except Exception as e:
+            print(e)
 
 
 if __name__ == "__main__":
